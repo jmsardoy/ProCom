6e9cef092ac1879c321db6408639ad3260208745
Juan Manuel Sardoy <jmsardoy@gmail.com>
2018-07-11 16:52:24 -0300

    qpsk fx

--- qpsk/qpsk_fixedp.py
+++ qpsk/qpsk_fixedp.py
@@ -3,7 +3,7 @@ from commpy.filters import rrcosfilter, rcosfilter
 
 from tool._fixedInt import *
 
-NCLK = 4*513
+NCLK = 4*512
 #seed_r = 0b110101010
 #seed_i = 0b111111110
 seed_r = 0x1aa
@@ -20,6 +20,14 @@ SAMPLE_RATE = BAUD_RATE*UPSAMPLE
 
 prbs_r_v = []
 prbs_i_v = []
+tx_r_v = []
+tx_i_v = []
+tx_r_in = [0]*NBAUDS*UPSAMPLE
+tx_i_in = [0]*NBAUDS*UPSAMPLE
+rx_r_v = []
+rx_i_v = []
+rx_r_in = [0]*NBAUDS*UPSAMPLE
+rx_i_in = [0]*NBAUDS*UPSAMPLE
 
 
 def prbs9(seed):    
@@ -27,6 +35,12 @@ def prbs9(seed):
     prbs_5 = int((seed & 0x010)>0)
     prbs = (((prbs_9^0b1^prbs_5)<<9) + seed>>1)
     return prbs
+
+def coef_mult(coef, values):
+    print coef, values
+    return np.inner(coef, values)
+    
+    
     
 def main():
     prbs_r = seed_r
@@ -37,18 +51,13 @@ def main():
     rrcos_fixed = arrayFixedInt(32,16,rrcos)
 
     fvalues = [r.fValue for r in rrcos_fixed]
-    for i in zip(rrcos, fvalues):
-        print i
-
-    tx_delay = 1
-    rx_delay = 2
-    
 
     for clk in range(NCLK):
 
         #FSM
         prbs9_enable = (clk%4==0)
         tx_enable = (clk%1==0)
+        rx_enable = (clk%1==0)
 
 
         #PRBS9
@@ -60,10 +69,28 @@ def main():
 
         #TX
         if tx_enable:
-            coef = rrcos_fixed[clk%4::UPSAMPLE]
-            print range(clk%4,24,UPSAMPLE)
-            #print coef
-            pass
+            coef = rrcos[clk%4::UPSAMPLE]
+            values_r = tx_r_in[clk%4::UPSAMPLE]
+            values_i = tx_i_in[clk%4::UPSAMPLE]
+            tx_r_v.append(coef_mult(coef, values_r))
+            tx_i_v.append(coef_mult(coef, values_i))
+            tx_r_in.insert(0, prbs_r_v[-1])
+            tx_r_in.pop()
+            tx_i_in.insert(0, prbs_i_v[-1])
+            tx_i_in.pop()
+
+        if rx_enable:
+            coef = rrcos[clk%4::UPSAMPLE]
+            values_r = rx_r_in[clk%4::UPSAMPLE]
+            values_i = rx_i_in[clk%4::UPSAMPLE]
+            rx_r_v.append(coef_mult(coef, values_r))
+            rx_i_v.append(coef_mult(coef, values_i))
+            rx_r_in.insert(0, tx_r_v[-1])
+            rx_r_in.pop()
+            rx_i_in.insert(0, tx_i_v[-1])
+            rx_i_in.pop()
+
+    import pdb; pdb.set_trace()
             
 
 
