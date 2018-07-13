82ce09425db269f4ce704ed300e6626048a17463
Juan Manuel Sardoy <jmsardoy@gmail.com>
2018-07-13 16:26:52 -0300

    punto fijo completado

--- qpsk/qpsk_fixedp.py
+++ qpsk/qpsk_fixedp.py
@@ -3,15 +3,15 @@ import matplotlib.pyplot as plt
 from commpy.filters import rrcosfilter, rcosfilter
 
 from tool._fixedInt import *
+from tool.DSPtools import *
 
 SEQ_LEN = 511
-NCLK = 4*SEQ_LEN
+NCLK = 1*4*SEQ_LEN
 #seed_r = 0b110101010
 #seed_i = 0b111111110
 seed_r = 0x1aa
 seed_i = 0x1fe
 
-LEN_SIGNALX = 511
 ROLL_OFF = 0.5
 NBAUDS = 6
 
@@ -19,9 +19,9 @@ UPSAMPLE = 4
 BAUD_RATE = 25e6
 SAMPLE_RATE = BAUD_RATE*UPSAMPLE
 
-TX_OFFSET = 3
+TX_OFFSET = 0
 
-DX_SWITCH_SEL = 2
+DX_SWITCH_SEL = 3
 
 
 def prbs9(seed):    
@@ -34,14 +34,14 @@ def prbs9(seed):
 def conv_tx(coef, values, clk, offset, upsample):
     coef = coef[(clk+offset)%4::upsample]
     values = values[(clk+offset)%4::upsample]
-    out = DeFixedInt(16,8)
+    out = DeFixedInt(9,7)
     for c, v in zip(coef, values):
         if v==1: out.value = (out+c).fValue
         else: out.value = (out-c).fValue
     return out
 
 def conv_rx(coef, values):
-    out = DeFixedInt(16,8)
+    out = DeFixedInt(9,7)
     for c, v in zip(coef, values):
         out.value = (out+(c*v)).fValue
     return out
@@ -53,8 +53,10 @@ def main():
     rst = 0
     rrcos = rrcosfilter(NBAUDS*UPSAMPLE, ROLL_OFF, 1./BAUD_RATE, SAMPLE_RATE)[1]
     rrcos = rrcos/np.sqrt(UPSAMPLE)
-    rrcos_fixed = arrayFixedInt(16,8,rrcos)
+    rrcos_fixed = arrayFixedInt(8,7,rrcos)
 
+    vector_r = []
+    vector_i = []
 
     prbs_r_v = []
     prbs_i_v = []
@@ -73,21 +75,24 @@ def main():
             tx_r_in = [0]*NBAUDS*UPSAMPLE
             tx_i_in = [0]*NBAUDS*UPSAMPLE
             
-            rx_r_in = [DeFixedInt(16,8)]*NBAUDS*UPSAMPLE
-            rx_i_in = [DeFixedInt(16,8)]*NBAUDS*UPSAMPLE
+            rx_r_in = [DeFixedInt(9,7)]*NBAUDS*UPSAMPLE
+            rx_i_in = [DeFixedInt(9,7)]*NBAUDS*UPSAMPLE
 
             prbs_r_s = 0
             prbs_i_s = 0
             prbs_r_p = 0
             prbs_i_p = 0
-            tx_r_s = DeFixedInt(16,8)
-            tx_i_s = DeFixedInt(16,8)
-            tx_r_p = DeFixedInt(16,8)
-            tx_i_p = DeFixedInt(16,8)
-            rx_r_s = DeFixedInt(16,8)
-            rx_i_s = DeFixedInt(16,8)
-            rx_r_p = DeFixedInt(16,8)
-            rx_i_p = DeFixedInt(16,8)
+
+            tx_r_s = DeFixedInt(9,7)
+            tx_i_s = DeFixedInt(9,7)
+            tx_r_p = DeFixedInt(9,7)
+            tx_i_p = DeFixedInt(9,7)
+
+            rx_r_s = DeFixedInt(9,7)
+            rx_i_s = DeFixedInt(9,7)
+            rx_r_p = DeFixedInt(9,7)
+            rx_i_p = DeFixedInt(9,7)
+            
             dx_r_s = 0
             dx_i_s = 0
             dx_r_p = 0
@@ -112,6 +117,7 @@ def main():
         tx_enable = (clk%1==0)
         rx_enable = (clk%1==0)
         dx_enable = ((clk+DX_SWITCH_SEL) % UPSAMPLE == 0)
+        ber_enable = clk%UPSAMPLE == 0
 
 
         #PRBS9
@@ -147,56 +153,45 @@ def main():
 
         #Slicer (No tiene retardo)
         if dx_enable:
-            print rx_r_s.fValue
+            vector_r.append(rx_r_s.fValue)
+            vector_i.append(rx_i_s.fValue)
             dx_r_s = int(rx_r_s.fValue > 0)
             dx_i_s = int(rx_i_s.fValue > 0)
         
 
-        """
-            ber_counter = 0
-            ber_tx_r_sr = [0]*2*SEQ_LEN
-            ber_tx_i_sr = [0]*2*SEQ_LEN
-            ber_dx = 0
-            ber_shift = 0
-            ber_error_count = 0
-            ber_adapt_flag = True
-            ber_min_error = 9999
-            ber_min_shift = 0
-        ber_enable = clk%4 == 0
         #BER Counter
         if ber_enable:
 
+            #Actualizo el buffer de simbolos enviados
+            ber_tx_r_sr.insert(0, prbs_r_p)
+            ber_tx_r_sr.pop()
+            ber_tx_i_sr.insert(0, prbs_i_p)
+            ber_tx_i_sr.pop()
             #Etapa de adaptacion
             if ber_adapt_flag:
-                if ber_counter<511:
-                    ber_error_count += ber_tx_r_sr[ber_shift] ^ ber_dx_r
-                    print ber_error_count, ber_tx_r_sr[ber_shift], ber_dx_r
-                    ber_error_count += ber_tx_i_sr[ber_shift] ^ ber_dx_i
-                    print ber_error_count, ber_tx_i_sr[ber_shift], ber_dx_i
-                    import pdb; pdb.set_trace()
+                if ber_counter<SEQ_LEN:
+                    ber_error_count += ber_tx_r_sr[ber_shift] ^ dx_r_p
+                    ber_error_count += ber_tx_i_sr[ber_shift] ^ dx_i_p
                     ber_counter += 1
                 else:
-                    #if ber_error_count == 0:
+                    print "............................."
+                    print ber_shift, ber_error_count
+                    print ber_min_shift, ber_min_error
                     if ber_error_count < ber_min_error:
                         ber_min_error = ber_error_count
-
                         ber_min_shift = ber_shift
-                    #ber_adapt_flag = False
-                    #else:
                     ber_counter = 0
                     ber_error_count = 0
                     ber_shift += 1
-                    print ber_min_shift, ber_min_error
-                if ber_shift == 511:
+                if ber_shift == 8:
                     ber_adapt_flag = False
-            ber_tx_r_sr.insert(0, prbs_r_v[-1])
-            ber_tx_r_sr.pop()
-            ber_tx_i_sr.insert(0, prbs_i_v[-1])
-            ber_tx_i_sr.pop()
-            #print ber_error_count, ber_shift
-        """
+                    ber_error_count = 0
+            else:
+                ber_error_count += ber_tx_r_sr[ber_min_shift] ^ dx_r_p
+                ber_error_count += ber_tx_i_sr[ber_min_shift] ^ dx_i_p
 
-
+                
+                
         #Actualizacion de los registros
         prbs_r_p = prbs_r_s
         prbs_i_p = prbs_i_s
@@ -219,17 +214,28 @@ def main():
             
             
 
-    print prbs_r_v[0:80*4:4]
-    print dx_r_v[::4][7:87]
-    print np.array(prbs_r_v[0:-7*4:4]) ^ np.array(dx_r_v[::4][7:])
-    print len(prbs_r_v[0:-7*4:4]), len(dx_r_v[::4][7:])
-    plt.figure(0)
+    plt.figure()
+    plt.grid()
     plt.plot([i.fValue for i in tx_r_v[:200]])
-    plt.figure(1)
+    
+    eyediagram([i.fValue for i in rx_r_v[12:]], 4, 1, UPSAMPLE)
+
+    plt.figure()
+    plt.grid()
     plt.plot([i.fValue for i in rx_r_v[:200]])
-    plt.figure(2)
-    plt.plot(dx_r_v, dx_i_v, '.')
+
+    rrcos_float = [i.fValue for i in rrcos_fixed]
+    H,A,F = resp_freq(rrcos_float, 1./BAUD_RATE, 512)
+    plt.figure()
+    plt.grid()
+    plt.semilogx(F, 20*np.log(H))
+
+    plt.figure()
+    plt.grid()
+    plt.plot(vector_r, vector_i, '.')
+
     plt.show()
+
             
 
 
